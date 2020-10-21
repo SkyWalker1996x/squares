3d8e5bf96e2e2a39c5e21b6400eee5008932342a
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-10-21 23:50:53 +0300

    feat: add visibility and invisibility for remove buttons - add event listener for remove buttons and complete the condition of mouseover for table

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Initial the task')
+const rows = document.querySelectorAll('.row');
+const table = document.querySelector('.table');
+const removeRow = document.querySelector('.remove-row')
+const removeCol = document.querySelector('.remove-col')
+
+rows.forEach((item) => {
+    console.log(item.children)
+});
+
+table.addEventListener('mouseover', (e) => {
+    removeRow.style.display = 'block';
+    removeCol.style.display = 'block';
+    console.log(e);
+})
+
+table.addEventListener('mouseout', () => {
+    removeRow.style.display = 'none';
+    removeCol.style.display = 'none';
+})
+
