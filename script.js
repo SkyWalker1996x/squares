2e5144f524e394e23dcb25647a688c4f4a2d81ab
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-11-05 17:47:25 +0200

    refactor: change approach from block-model to table-model

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const table = document.querySelector(".table");
+const tableRef = document.getElementById("tableID");
+const rowsCollection = tableRef.rows;
+const tableWrapper = document.querySelector(".table-wrapper");
 const deleteRowButton = document.querySelector(".remove-row");
 const deleteColButton = document.querySelector(".remove-col");
 const addRowButton = document.querySelector(".add-row");
 const addColButton = document.querySelector(".add-col");
-const cellWidth = document.querySelector(".cell").offsetWidth;
-const cellHeight = document.querySelector(".cell").offsetHeight;
-let removeColId;
+let removeCellId;
 let removeRowId;
 
 const hiddenRemoveButton = () => {
@@ -13,55 +13,41 @@ const hiddenRemoveButton = () => {
   deleteColButton.style.display = "none";
 };
 const handleRowAppend = () => {
-  const rows = document.querySelectorAll(".row");
-  const newRow = rows[0].cloneNode(true);
-  table.appendChild(newRow);
+  const newRow = tableRef.insertRow(-1);
+  newRow.classList.add("row");
+  newRow.innerHTML = rowsCollection[0].innerHTML;
 };
 const handleColAppend = () => {
-  const rows = document.querySelectorAll(".row");
-  const cell = document.querySelector(".cell");
-  rows.forEach((item) => {
-    item.append(cell.cloneNode(true));
-  });
+  for (let rowRef of rowsCollection) {
+    const newCell = rowRef.insertCell(-1);
+    newCell.classList.add("cell");
+  }
 };
 const handleRowRemove = () => {
-  const rows = document.querySelectorAll(".row");
-  deleteRowButton.style.display = "none";
-  rows[removeRowId].remove();
+  hiddenRemoveButton();
+  tableRef.deleteRow(removeRowId);
 };
 const handleColRemove = () => {
-  const rowsCollection = document.querySelectorAll(".row");
   hiddenRemoveButton();
-  rowsCollection.forEach((i, index) => {
-    const rowItem = rowsCollection[index];
-    const removeCellItem = rowItem.querySelectorAll(".cell")[removeColId];
-    removeCellItem.remove();
-  });
+  for (let rowRef of rowsCollection) {
+    rowRef.deleteCell(removeCellId);
+  }
 };
 const onMouseoverTable = (e) => {
-  const rows = document.querySelectorAll(".row");
-  const cellsRow = document.querySelector(".row").querySelectorAll(".cell");
+  deleteRowButton.style.display = rowsCollection.length <= 1 ? "none" : "block";
+  deleteColButton.style.display = rowsCollection[0].childElementCount <= 1 ? "none" : "block";
+  const { target } = e;
 
-  deleteRowButton.style.display = rows.length <= 1 ? "none" : "block";
-  deleteColButton.style.display = cellsRow.length <= 1 ? "none" : "block";
-
-  if (e.target.className === "cell") {
-    const cellsArray = [...e.target.parentElement.children];
-    const indexCell = cellsArray.indexOf(e.target);
-    deleteColButton.style.left = `${indexCell * cellWidth}px`;
-    removeColId = indexCell;
-
-    const rowsArray = [...e.target.parentElement.parentElement.children].filter(
-      (item) => item.className === "row"
-    );
-    const indexRow = rowsArray.indexOf(e.target.parentElement);
-    deleteRowButton.style.top = `${indexRow * cellHeight}px`;
-    removeRowId = indexRow;
+  if (target.className === "cell") {
+    removeCellId = target.cellIndex;
+    deleteColButton.style.left = `${target.offsetLeft}px`;
+    removeRowId = target.parentNode.rowIndex;
+    deleteRowButton.style.top = `${target.offsetTop}px`;
   }
 };
 
-table.addEventListener("mouseover", onMouseoverTable);
-table.addEventListener("mouseout", hiddenRemoveButton);
+tableWrapper.addEventListener("mouseover", onMouseoverTable);
+tableWrapper.addEventListener("mouseout", hiddenRemoveButton);
 addRowButton.addEventListener("click", handleRowAppend);
 addColButton.addEventListener("click", handleColAppend);
 deleteRowButton.addEventListener("click", handleRowRemove);
