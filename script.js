d954d1f36f97399972ec46db581f096ce19173c1
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-11-05 23:06:35 +0200

    refactor: implement method to initial event listeners

--- script.js
+++ script.js
@@ -1,62 +1,7 @@
-/*const tableRef = document.getElementById("tableID");
-const rowsCollection = tableRef.rows;
-const tableWrapper = document.querySelector(".table-wrapper");
-const deleteRowButton = document.querySelector(".remove-row");
-const deleteColButton = document.querySelector(".remove-col");
-const addRowButton = document.querySelector(".add-row");
-const addColButton = document.querySelector(".add-col");
-let removeCellId;
-let removeRowId;
-
-const hiddenRemoveButton = () => {
-  deleteRowButton.style.display = "none";
-  deleteColButton.style.display = "none";
-};
-const handleRowAppend = () => {
-  const newRow = tableRef.insertRow(-1);
-  newRow.classList.add("row");
-  newRow.innerHTML = rowsCollection[0].innerHTML;
-};
-const handleColAppend = () => {
-  for (let rowRef of rowsCollection) {
-    const newCell = rowRef.insertCell(-1);
-    newCell.classList.add("cell");
-  }
-};
-const handleRowRemove = () => {
-  hiddenRemoveButton();
-  tableRef.deleteRow(removeRowId);
-};
-const handleColRemove = () => {
-  hiddenRemoveButton();
-  for (let rowRef of rowsCollection) {
-    rowRef.deleteCell(removeCellId);
-  }
-};
-const onMouseoverTable = (e) => {
-  deleteRowButton.style.display = rowsCollection.length <= 1 ? "none" : "block";
-  deleteColButton.style.display =
-    rowsCollection[0].childElementCount <= 1 ? "none" : "block";
-  const { target } = e;
-
-  if (target.className === "cell") {
-    removeCellId = target.cellIndex;
-    deleteColButton.style.left = `${target.offsetLeft}px`;
-    removeRowId = target.parentNode.rowIndex;
-    deleteRowButton.style.top = `${target.offsetTop}px`;
-  }
-};
-
-tableWrapper.addEventListener("mouseover", onMouseoverTable);
-tableWrapper.addEventListener("mouseout", hiddenRemoveButton);
-addRowButton.addEventListener("click", handleRowAppend);
-addColButton.addEventListener("click", handleColAppend);
-deleteRowButton.addEventListener("click", handleRowRemove);
-deleteColButton.addEventListener("click", handleColRemove);*/
-
 class Table {
   constructor(selector, sizeRows, sizeColumns) {
     this.initTableElements(selector, sizeRows, sizeColumns);
+    this.initEventListeners();
   }
 
   initTableElements = (selector, sizeRows, sizeColumns) => {
@@ -85,6 +30,11 @@ class Table {
         className: "remove-col",
         textContent: "-",
       },
+      {
+        buttonName: "deleteListenersButton",
+        className: "remove-listeners",
+        textContent: "RL",
+      },
     ];
 
     this.tableWrapper = document.createElement("div");
@@ -93,6 +43,7 @@ class Table {
     this.tableWrapper.classList.add("table-wrapper");
     this.table.classList.add("table");
     this.table.id = this.selector;
+    this.rowsCollection = this.table.rows;
 
     document.body.append(this.tableWrapper);
     this.tableWrapper.append(this.table);
@@ -101,14 +52,22 @@ class Table {
     this.createTableElements(this.sizeRows, this.sizeColumns);
   };
 
-  initEventListeners = () => {};
+  initEventListeners = () => {
+    this.tableWrapper.addEventListener("mouseover", this.onMouseoverTable.bind(this));
+    this.tableWrapper.addEventListener("mouseout", this.hiddenRemoveButton.bind(this));
+    this.addRowButton.addEventListener("click", this.handleRowAppend.bind(this));
+    this.addColButton.addEventListener("click", this.handleColAppend.bind(this));
+    this.deleteRowButton.addEventListener("click", this.handleRowRemove.bind(this));
+    this.deleteColButton.addEventListener("click", this.handleColRemove.bind(this));
+    this.deleteListenersButton.addEventListener('click', this.handleEventListenersRemove.bind(this));
+  };
 
   createButtons = (buttons) => {
     buttons.forEach((button) => {
       const { buttonName, className, textContent } = button;
 
       this[buttonName] = document.createElement("button");
-      this[buttonName].classList.add(className, "button");
+      this[buttonName].classList.add("button", className);
       this[buttonName].textContent = textContent;
       this.tableWrapper.append(this[buttonName]);
     });
@@ -124,6 +83,61 @@ class Table {
       }
     }
   };
+
+  hiddenRemoveButton = () => {
+    this.deleteRowButton.style.display = "none";
+    this.deleteColButton.style.display = "none";
+  };
+
+  handleRowAppend = () => {
+    const newRow = this.table.insertRow(-1);
+    newRow.classList.add("row");
+    newRow.innerHTML = this.rowsCollection[0].innerHTML;
+  };
+
+  handleColAppend = () => {
+    for (let rowRef of this.rowsCollection) {
+      const newCell = rowRef.insertCell(-1);
+      newCell.classList.add("cell");
+    }
+  };
+
+  handleRowRemove = () => {
+    this.hiddenRemoveButton();
+    this.table.deleteRow(this.removeRowId);
+  };
+
+  handleColRemove = () => {
+    this.hiddenRemoveButton();
+    for (let rowRef of this.rowsCollection) {
+      rowRef.deleteCell(this.removeCellId);
+    }
+  };
+
+  onMouseoverTable = (e) => {
+    this.deleteRowButton.style.display =
+      this.rowsCollection.length <= 1 ? "none" : "block";
+    this.deleteColButton.style.display =
+      this.rowsCollection[0].childElementCount <= 1 ? "none" : "block";
+    const { target } = e;
+
+    if (target.className === "cell") {
+      this.removeCellId = target.cellIndex;
+      this.deleteColButton.style.left = `${target.offsetLeft}px`;
+      this.removeRowId = target.parentNode.rowIndex;
+      this.deleteRowButton.style.top = `${target.offsetTop}px`;
+    }
+  };
+
+  /*handleEventListenersRemove = () => {
+    const oldTable = this.tableWrapper;
+    const newTable = oldTable.cloneNode(true);
+    oldTable.parentNode.replaceChild(newTable, oldTable);
+
+    this.tableWrapper = newTable;
+  }*/
 }
 
-const newTable = new Table("new-table", 4, 4);
+const firstTable = new Table("firstTable", 4, 4);
+const secondTable = new Table("secondTable", 5, 5);
+
