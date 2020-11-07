12f89729aeb704d25a183bc42570901ed54da85a
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-11-07 15:07:21 +0200

    refactor: change structure of class - add to constructor elements of class's interface; - change naming of variables: rowsCollection to rows, sizeRows to rowsNumbers; - change parameters of class to object.

--- script.js
+++ script.js
@@ -1,80 +1,93 @@
+const buttons = [
+  {
+    buttonName: "addRowButton",
+    className: "add-row",
+    textContent: "+",
+  },
+  {
+    buttonName: "addColButton",
+    className: "add-col",
+    textContent: "+",
+  },
+  {
+    buttonName: "deleteRowButton",
+    className: "remove-row",
+    textContent: "-",
+  },
+  {
+    buttonName: "deleteColButton",
+    className: "remove-col",
+    textContent: "-",
+  },
+  {
+    buttonName: "deleteListenersButton",
+    className: "remove-listeners",
+    textContent: "RL",
+  },
+];
+
 class Table {
-  constructor(selector, sizeRows, sizeColumns) {
-    this.initTableElements(selector, sizeRows, sizeColumns);
+  constructor(tableConfig) {
+    const { selector, ...tableSize } = tableConfig;
+
+    this.parent = document.getElementById(selector);
+    this.table = document.createElement("table");
+    this.rows = this.table.rows;
+    this.addRowButton = null;
+    this.addColButton = null;
+    this.deleteRowButton = null;
+    this.deleteColButton = null;
+    this.deleteListenersButton = null;
+    this.removeRowId = -1;
+    this.removeCellId = -1;
+
+    this.initTableElements(tableSize);
     this.initEventListeners();
   }
 
-  initTableElements = (selector, sizeRows, sizeColumns) => {
-    const buttons = [
-      {
-        buttonName: "addRowButton",
-        className: "add-row",
-        textContent: "+",
-      },
-      {
-        buttonName: "addColButton",
-        className: "add-col",
-        textContent: "+",
-      },
-      {
-        buttonName: "deleteRowButton",
-        className: "remove-row",
-        textContent: "-",
-      },
-      {
-        buttonName: "deleteColButton",
-        className: "remove-col",
-        textContent: "-",
-      },
-      {
-        buttonName: "deleteListenersButton",
-        className: "remove-listeners",
-        textContent: "RL",
-      },
-    ];
-
-    this.createTable(selector);
-    this.createButtons(buttons);
-    this.createTableElements(sizeRows, sizeColumns);
+  initTableElements = (tableSize) => {
+    this.createTableLayout();
+    this.createButtons();
+    this.createTableElements(tableSize);
   };
 
   initEventListeners = () => {
-    this.tableWrapper.addEventListener("mouseover", this.onMouseoverTable);
-    this.tableWrapper.addEventListener("mouseout", this.hiddenRemoveButton);
+    this.parent.addEventListener("mouseover", this.onMouseoverTable);
+    this.parent.addEventListener("mouseout", this.hiddenRemoveButton);
     this.addRowButton.addEventListener("click", this.handleRowAppend);
     this.addColButton.addEventListener("click", this.handleColAppend);
     this.deleteRowButton.addEventListener("click", this.handleRowRemove);
     this.deleteColButton.addEventListener("click", this.handleColRemove);
-    this.deleteListenersButton.addEventListener('click', this.handleEventListenersRemove);
+    this.deleteListenersButton.addEventListener(
+      "click",
+      this.handleEventListenersRemove
+    );
   };
 
-  createButtons = (buttons) => {
+  createButtons = () => {
     buttons.forEach((button) => {
       const { buttonName, className, textContent } = button;
 
       this[buttonName] = document.createElement("button");
       this[buttonName].classList.add("button", className);
       this[buttonName].textContent = textContent;
-      this.tableWrapper.append(this[buttonName]);
+      this.parent.append(this[buttonName]);
     });
   };
 
-  createTable = (selector) => {
-    this.tableWrapper = document.createElement("div");
-    this.table = document.createElement("table");
-    this.tableWrapper.classList.add("table-wrapper");
+  createTableLayout = () => {
     this.table.classList.add("table");
-    this.table.id = selector;
-    this.rowsCollection = this.table.rows;
-    document.body.append(this.tableWrapper);
-    this.tableWrapper.append(this.table);
+    this.parent.classList.add("table-wrapper");
+    this.parent.append(this.table);
   };
 
-  createTableElements = (sizeRows, sizeColumns) => {
-    for (let i = 0; i < sizeRows; i++) {
+  createTableElements = (tableSize) => {
+    const { rowsNumber, columnsNumber } = tableSize;
+
+    for (let i = 0; i < rowsNumber; i++) {
       const newRow = this.table.insertRow(-1);
       newRow.classList.add("row");
-      for (let k = 0; k < sizeColumns; k++) {
+      for (let k = 0; k < columnsNumber; k++) {
         const newCell = newRow.insertCell(-1);
         newCell.classList.add("cell");
       }
@@ -89,11 +102,11 @@ class Table {
   handleRowAppend = () => {
     const newRow = this.table.insertRow(-1);
     newRow.classList.add("row");
-    newRow.innerHTML = this.rowsCollection[0].innerHTML;
+    newRow.innerHTML = this.rows[0].innerHTML;
   };
 
   handleColAppend = () => {
-    for (let rowRef of this.rowsCollection) {
+    for (let rowRef of this.rows) {
       const newCell = rowRef.insertCell(-1);
       newCell.classList.add("cell");
     }
@@ -106,16 +119,16 @@ class Table {
 
   handleColRemove = () => {
     this.hiddenRemoveButton();
-    for (let rowRef of this.rowsCollection) {
+    for (let rowRef of this.rows) {
       rowRef.deleteCell(this.removeCellId);
     }
   };
 
   onMouseoverTable = (e) => {
     this.deleteRowButton.style.display =
-      this.rowsCollection.length <= 1 ? "none" : "block";
+      this.rows.length <= 1 ? "none" : "block";
     this.deleteColButton.style.display =
-      this.rowsCollection[0].childElementCount <= 1 ? "none" : "block";
+      this.rows[0].childElementCount <= 1 ? "none" : "block";
     const { target } = e;
 
     if (target.className === "cell") {
@@ -127,12 +140,17 @@ class Table {
   };
 
   handleEventListenersRemove = () => {
-    const oldTable = this.tableWrapper;
+    const oldTable = this.parent;
     const newTable = oldTable.cloneNode(true);
     oldTable.parentNode.replaceChild(newTable, oldTable);
-    this.tableWrapper = newTable;
+    this.parent = newTable;
   };
 }
 
-const firstTable = new Table("firstTable", 4, 4);
-const secondTable = new Table("secondTable", 5, 5);
+const tableConfig = {
+  selector: "table",
+  rowsNumber: 7,
+  columnsNumber: 7,
+};
+
+const firstTable = new Table(tableConfig);
